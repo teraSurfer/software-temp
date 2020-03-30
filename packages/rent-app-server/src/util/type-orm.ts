01cc4b3105073364e0541a63fe49fa9a5e46662d
teraSurfer <achalaesh@live.com>
2020-03-29 18:17:54 -0700

    minor fix

--- packages/rent-app-server/src/util/type-orm.ts
+++ packages/rent-app-server/src/util/type-orm.ts
@@ -4,4 +4,4 @@ export const createDBConnection = async () => {
     const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
     console.log(connectionOptions.database);
     await createConnection({...connectionOptions, name: 'default'});
-}
+};
